6008964e06f3e92ae65c9f77596e19117b3e8c8d
张永鹏 <407423058zyp@gmail.com>
2017-04-01 01:02:34 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,12 @@ class User < ApplicationRecord
          has_many :groups
          has_many :posts
          has_many :group_relationships
-  has_many :participated_groups, :through => :group_relationships, :source => :group
+         has_many :participated_groups, :through => :group_relationships, :source => :group
 
       def is_member_of?(group)
         participated_groups.include?(group)
       end
+
       def join!(group)
     participated_groups << group
   end
@@ -19,4 +20,5 @@ class User < ApplicationRecord
   def quit!(group)
     participated_groups.delete(group)
   end
+
 end
